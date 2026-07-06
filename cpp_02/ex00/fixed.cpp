2b339a9083813a3a1bd59caa3ffd57422c5d1196
Youssef El Mohajir <yel-moha@c2r1p12.42firenze.it>
2026-07-06 15:07:32 +0200

    Comments about new syntax like: The copy constructor the assignment oprator

--- cpp_02/ex00/fixed.cpp
+++ cpp_02/ex00/fixed.cpp
@@ -2,18 +2,28 @@
 
 const int Fixed::fractionalBits = 8;
 
-Fixed::Fixed(void)
+Fixed::Fixed(void) // Costruttore di default
 {
     this->fixed = 0;
     std::cout << "Default constructor called" << std::endl;
 }
 
+// Costruttore di copia: Fixed::Fixed(const Fixed &other)
+// Fixed: È il nome della funzione.
+// Fixed::Fixed: Poiché si chiama come la classe, il compilatore sa che è un costruttore.
+// const Fixed &: ricevo una reference costante ad un altro oggetto Fixed.
+// const in questo caso perche' si limita a copiare il contenuto della classe senza modificarlo
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
     this->fixed = other.fixed;
 }
 
+// Operatore di assegnazione 
+// Fixed &: La funzione restituisce una reference ad un Fixed.
+// operator= È semplicemente il nome della funzione.
+//Il compilatore dice questa funzione rappresenta l'operatore =
+//Per questo possiamo scrivere a = b; e viene tradotto in a.operator=(b);Sono la stessa cosa.
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
