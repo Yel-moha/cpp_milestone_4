f7fab98486d4fcd8e7960dd051e9b37131b0c7c8
Youssef El Mohajir <yel-moha@c2r2p12.42firenze.it>
2026-07-20 15:56:10 +0200

    cpp_03 ex00

---
+++ cpp_03/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "ClapTrap.hpp"
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap claptrap("CL4P-TP");
+    claptrap.attack("un bersaglio");
+    claptrap.takeDamage(5);
+    claptrap.beRepaired(3);
+    return (0);
+}
